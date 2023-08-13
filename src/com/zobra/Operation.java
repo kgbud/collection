357e5533c278e6b70d60bc319e6670177bbeca2d
kgbud <kailashghimire54@gmail.com>
2023-08-13 17:43:50 -0500

    adding Arithmetic Project

---
+++ src/com/zobra/Operation.java
@@ -0,0 +1,32 @@
+package com.zobra;
+import java.util.Scanner;
+
+public class Operation {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter number");
+		int num = scanner.nextInt();
+
+		switch (num) {
+
+		case 10: //if (num == 10 && num > 20)
+			System.out.println("10 rupees dispatched");
+			break;
+
+		case 100://else if (num == 100)
+			System.out.println("100 rupees dispatched");
+			break;
+
+		case 200://else if (num ==200)
+			System.out.println("100 rupees dispatched");
+			break;
+
+		default://else
+			System.out.println("incorrect selection");
+		}
+
+	}
+
+
+
+}
